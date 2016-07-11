984e970676fb035f179235b864ee135f8ee95d8e
YanBingxin <775127481@qq.com>
2016-07-11 16:08:32 +0800

    优化结果显示
    优化结果显示

--- FilesCompare/Converter/JarNameConverter.cs
+++ FilesCompare/Converter/JarNameConverter.cs
@@ -6,22 +6,22 @@ using System.Windows.Data;
 
 namespace FilesCompare.Converter
 {
-    public class JarNameConverter : IValueConverter
+    public class JarNameConverter : IMultiValueConverter
     {
 
-        #region IValueConverter 成员
-
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        #region IMultiValueConverter 成员
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            string jarName = value as string;
+            string jarName = values[1].ToString().Replace(values[0].ToString(), "..");
             return string.IsNullOrEmpty(jarName) ? string.Empty : "[" + jarName + "]";
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotImplementedException();
         }
 
         #endregion
+
     }
 }
